6759b516f48bd0e891621d338495f1089c17937b
Lem Canady <lem.canady@gmail.com>
2023-07-16 09:45:24 -0700

    comtitle bug fix

--- src/services/commands/channels.ts
+++ src/services/commands/channels.ts
@@ -20,15 +20,15 @@ export const matchChannel = async (ctx: IContext) => {
     const channel = await chans.findOne({ name: chan?.channel });
 
     if (match) {
-      if (!flags.check(en.flags || "", channel?.lock || "")) return false;
+      if (!flags.check(en.flags + " " || "", channel?.lock || "")) return false;
       if (match[1] === ":") {
-        msg = `${chan?.title || ""} ${chan?.mask ? chan.mask : moniker(en)} ${
-          match[2]
-        }`;
+        msg = `${chan?.title + " " || ""}${
+          chan?.mask ? chan.mask : moniker(en)
+        } ${match[2]}`;
       } else if (match[1] === ";") {
-        msg = `${chan?.title || ""} ${chan?.mask ? chan.mask : moniker(en)}${
-          match[2]
-        }`;
+        msg = `${chan?.title + " " || ""}${
+          chan?.mask ? chan.mask : moniker(en)
+        }${match[2]}`;
       } else if (msg.toLowerCase() === "on" && chan?.active === false) {
         chan.active = true;
         ctx.socket.join(chan.channel);
