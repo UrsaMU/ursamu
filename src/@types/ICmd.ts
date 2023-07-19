526814efa81f149d29f11b3e5b272d023fa98eb1
Lem Canady <lem.canady@gmail.com>
2023-07-19 07:29:05 -0700

    added categiry to ICMD

--- src/@types/ICmd.ts
+++ src/@types/ICmd.ts
@@ -3,6 +3,7 @@ import { IContext } from "./IContext";
 export interface ICmd {
   name: string;
   help?: string;
+  category?: string;
   hidden?: boolean;
   pattern: string | RegExp;
   lock?: string;
