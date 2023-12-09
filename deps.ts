70daa4fa7edc8b067de070f36a67786fb3f39449
Neil Stevens <neil@hakubi.us>
2023-12-08 22:12:06 -0500

    Fixing every npm import but express-svelte, which doesn't work in deno

--- deps.ts
+++ deps.ts
@@ -1,11 +1,17 @@
 export { Tags } from "npm:@digibear/tags@1.0.0"
 export { Parser } from "npm:@ursamu/parser@1.2.4"
-export { compare, hash } from "npm:bcryptjs@2.4.3"
+import bcrypt from "npm:bcryptjs@2.4.3"
+export const compare = bcrypt.compare
+export const hash = bcrypt.hash
 export { config } from "npm:dotenv@16.3.1"
 export { NextFunction, Request, Response, Router, express } from "npm:express-svelte@1.0.8"
-export { jwt } from "npm:jsonwebtoken@9.0.1"
-export { set, get, isnumber, repeat } from "npm:lodash@4.17.21"
-export { Datastore } from "npm:nedb-promises@6.2.1"
+export jwt from "npm:jsonwebtoken@9.0.1"
+import lodash from "npm:lodash@4.17.21"
+export const set = lodash.set
+export const get = lodash.get
+export const isnumber = lodash.isnumber
+export const repeat = lodash.repeat
+export Datastore from "npm:nedb-promises@6.2.1"
 export { Socket, Server } from "npm:socket.io@4.7.1"
 export { io } from "npm:socket.io-client@4.7.1"
 export { VM } from "npm:vm2@3.9.19"
