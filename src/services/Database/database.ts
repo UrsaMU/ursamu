3c5bd349052726109275b7aa43044df049b66100
Neil Stevens <neil@hakubi.us>
2023-12-08 21:55:15 -0500

    nedb

--- src/services/Database/database.ts
+++ src/services/Database/database.ts
@@ -1,4 +1,4 @@
-import Datastore from "nedb-promises";
+import Datastore from "../../../deps.ts";
 import { IDBOBJ } from "../../@types/IDBObj.ts";
 import config from "../../ursamu.config.ts";
 import { IChannel } from "../../@types/Channels.ts";
