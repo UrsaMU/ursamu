2e5b9d65a47efa8083c1c0ac28f210482c6bf5fb
Neil Stevens <neil@hakubi.us>
2023-12-09 18:17:04 -0500

    Fix jwt export

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export { Tags } from "npm:@digibear/tags@1.0.0"
 export { Parser } from "npm:@ursamu/parser@1.2.4"
 export { config } from "npm:dotenv@16.3.1"
-export jwt from "npm:jsonwebtoken@9.0.1"
+export * as jwt from "npm:jsonwebtoken@9.0.1"
 export Datastore from "npm:nedb-promises@6.2.1"
 export { Socket, Server } from "npm:socket.io@4.7.1"
 export { io } from "npm:socket.io-client@4.7.1"
