d47d5f16e0daf558cf44e45d1e5bb260a945cb56
Neil Stevens <neil@hakubi.us>
2023-12-13 19:29:18 -0500

    Adding some temporary debug logging to database

--- src/services/Database/database.ts
+++ src/services/Database/database.ts
@@ -5,6 +5,12 @@ import { IChannel } from "../../@types/Channels.ts";
 import { IMail } from "../../@types/IMail.ts";
 import { IArticle, IBoard } from "../../@types/index.ts";
 
+function d(...args) {
+  const e = new Error();
+  const level = e.stack.split("\n")[3];
+  console.log(...args, level);
+}
+
 export class DBO<T> {
   db: any;
 
@@ -16,48 +22,62 @@ export class DBO<T> {
   }
 
   coll() {
-    return this.client.db().collection(this.collection)
+    return this.client.db().collection(this.collection);
   }
 
   async insert(data: T) {
     await this.coll().insertOne(data);
-    return this.find(data)
+    const ret = this.find(data);
+    d("[database insert]", data, ret);
+    return ret;
   }
 
   async find(query?: any) {
-    return (await this.coll().find(query)).toArray();
+    const ret = await (await this.coll().find(query)).toArray();
+    d("[database find]", query, ret);
+    return ret;
   }
 
   async findAll() {
-    return (await this.coll().find({})).toArray();
+    const ret = await (await this.coll().find({})).toArray();
+    d("[database findAll]", ret);
+    return ret;
   }
 
   async findOne(query: any) {
-    return await this.coll().findOne(query);
-  }
+    const ret = await this.coll().findOne(query);
+    d("[database findOne]", query, ret);
+    return ret;
+}
 
   async update(query: any, data: any) {
     try {
+      d("[database update]", query, data)
       await this.coll().update(query, data, {
         upsert: true,
       });
     } catch(e) {
       if(e.type == "MongoInvalidArgumentError") {
+        d("[database update replaceOne]", query, data)
         await this.coll().replaceOne(query, data, {
           upsert: true,
         });
       }
     }
-
-    return this.find(query);
+    const ret = this.find(query);
+    d("[database update]", ret);
+    return ret;
   }
 
   async remove(query: any) {
-    return await this.coll().deleteMany(query);
+    await this.coll().deleteMany(query);
+    d("[database remove]", query);
   }
 
   async count(query: any) {
-    return await this.coll().count(query);
+    const ret = await this.coll().count(query);
+    d("[database count]", query, ret);
+    return ret;
   }
 }
 
