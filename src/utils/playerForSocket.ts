ed81c1628277fd19746f86d0ca024f1e714618e8
Neil Stevens <neil@hakubi.us>
2023-12-17 18:11:27 -0500

    Ooops fix the playerForSocket copy/paste error

--- src/utils/playerForSocket.ts
+++ src/utils/playerForSocket.ts
@@ -3,4 +3,4 @@ import { dbojs } from "../services/Database/index.ts";
 export const playerForSocket = async (socket: IMSocket) => {
   const ret = await dbojs.query({ id: socket.cid });
   return ret.length ? ret[0] : false;
-});
+};
