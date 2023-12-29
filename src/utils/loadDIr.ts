26c091a2ffd6c4a3ed5454fdc7682fa40d52f9fd
Lem Canady <lem.canady@gmail.com>
2023-12-29 10:52:47 -0800

    Add URL support to plugins()

--- src/utils/loadDIr.ts
+++ src/utils/loadDIr.ts
@@ -1,13 +1,18 @@
 import { dfs, dpath } from "../../deps.ts";
 
-export async function plugins(dir: string) {
-  const entries = dfs.walk(dir, { match: [/\.ts$/, /\.js$/], maxDepth: 1 })
-  for await (const entry of entries) {
-    if (entry.isFile) {
-      // Dynamically import the module
-      const module = await import(dpath.toFileUrl(entry.path));
-      // If the module has a default export function, call it
-      module.default?.();
+export async function plugins(source: string) {
+  // Check if the source is a URL
+  if (source.startsWith("http://") || source.startsWith("https://")) {
+    const module = await import(source);
+    module.default?.();
+  } else {
+    // Handle local directory logic
+    const entries = dfs.walk(source, { match: [/\.ts$/, /\.js$/], maxDepth: 1 });
+    for await (const entry of entries) {
+      if (entry.isFile) {
+        const module = await import(dpath.toFileUrl(entry.path).toString());
+        module.default?.();
+      }
     }
   }
 }
