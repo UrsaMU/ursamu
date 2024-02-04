f9a96f3bbade781b376e2506d55753bed26ce596
Lem Canady <lem.canady@gmail.com>
2024-02-04 03:40:49 -0800

    testing

--- mod.ts
+++ mod.ts
@@ -2,3 +2,4 @@ export * from "./src/utils/index.ts";
 export * from "./src/services/index.ts";
 export * from "./src/@types/index.ts";
 export * from "./src/app.ts";
+export * from "./src/config.ts";
