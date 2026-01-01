e7421678ab58674beed478d353f627e847d6dfb6
lcanady <lem.canady@gmail.com>
2026-01-01 07:13:14 -0800

    feat: Add `@tailwindcss/typography` and Lume `relative_urls` plugin, removing manual URL filtering.

--- docs/_config.ts
+++ docs/_config.ts
@@ -9,6 +9,8 @@ import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
 import date from "lume/plugins/date.ts";
 import nunjucks from "lume/plugins/nunjucks.ts";
+import relative_urls from "lume/plugins/relative_urls.ts";
+import typography from "@tailwindcss/typography";
 
 const site = lume({
   src: "./",
@@ -41,11 +43,13 @@ site
           },
         },
       },
+      plugins: [typography],
     },
   }))
   .use(postcss())
   .use(date())
   .use(nunjucks())
+  .use(relative_urls())
   .copy("assets")
   .copy("init.ts")
   .ignore("README.md", "deno.json", "_site", "node_modules");
