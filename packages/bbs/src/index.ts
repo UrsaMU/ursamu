74df5d7bc0fc3a3d1fce83d2370cf861ee9fe71b
lcanady <lem.canady@gmail.com>
2026-07-16 09:40:02 -0700

    fix: update dependencies and add preflight task in BBS package

--- packages/bbs/src/index.ts
+++ packages/bbs/src/index.ts
@@ -1,6 +1,6 @@
 import type { IPlugin } from "@ursamu/mush";
 import { registerPluginRoute } from "@ursamu/mush";
-import { registerHelpDir } from "@ursamu/help-plugin";
+import { registerHelpDir } from "@ursamu/help";
 import { bboardsRouteHandler } from "./router.ts";
 import { startCleanupInterval, stopCleanupInterval } from "./cleanup.ts";
 import { seedDefaultBoards } from "./seed.ts";
